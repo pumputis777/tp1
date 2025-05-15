22218c0b2f43afc85d8d60d7aef615ba334323f1
pumputis777 <facundopumputis@gmail.com>
2025-05-15 19:30:29 -0300

    Update main (5).cpp

--- main (5).cpp
+++ main (5).cpp
@@ -3,22 +3,21 @@
 using namespace std;
 int main()
 {
-    // Declarar las variables
-    float precio;
-    float total;
-    int cantidad;
 
-    // Solicitar al usuario que ingrese el precio y la cantidad
+    float precio; // Declaro la primera variable
+    float total; // Declaro la segunda variable
+    int cantidad; // Declaro la tercera variable
+    
+    cout << "este programa ve cuanto es el total a abonar"; // indica lo que hace el programa al usuario
+    // Solicita al usuario que ingrese el precio y la cantidad
     cout << "Ingrese el precio del artículo: ";
     cin >> precio;
     cout << "Ingrese la cantidad que desea comprar: ";
     cin >> cantidad;
 
-    // Calcular el total a abonar
-    total = precio * cantidad;
+    total = precio * cantidad; // Calcula el total a abonar y guarda el resultado en la variable total
 
-    // Mostrar el resultado
-    cout << "El total a abonar es: $" << total << endl;
+    cout << "El total a abonar es: $" << total << endl; // Muestra el resultado a abonar
 
 
     return 0;
