60b03d66349c25d0359ec91c45d0786ef76bec22
pumputis777 <facundopumputis@gmail.com>
2025-05-15 19:23:04 -0300

    Update main (3).cpp

--- main (3).cpp
+++ main (3).cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int main()
 {
-// Declarar las variables
-    float num1;
-    float num2;
-    float num3;
-    float num4;
-    float suma;
-    float producto;
-    cout << "este programa suma los dos primeros numeros y multiplica los ultimos dos";
+
+    float num1; // Declaro la primera variable
+    float num2; // Declaro la segunda variable
+    float num3; // Declaro la tercera variable
+    float num4; // Declaro la cuarta variable
+    float suma; // Declaro la quinta variable
+    float producto; // Declaro la sexta variable
+    cout << "este programa suma los dos primeros numeros y multiplica los ultimos dos"; //le indica al usuario lo que hace el programa
     // Solicitar al usuario que ingrese los cuatro números
     cout << "Ingrese el primer número: ";
     cin >> num1;
@@ -21,14 +21,12 @@ int main()
     cout << "Ingrese el cuarto número: ";
     cin >> num4;
 
-    // Calcular la suma de los dos primeros números
-    suma = num1 + num2;
+    suma = num1 + num2; // Calcula la suma de los dos primeros números y lo guarda en la variable suma
 
-    // Calcular el producto del tercero y cuarto número
-    producto = num3 * num4;
+    producto = num3 * num4; // Calcula el producto de los dos ultimos números y lo guarda en la variable producto
 
     // Mostrar los resultados
-    cout << "La suma de los dos primeros números es: " << suma << endl;
-    cout << "El producto del tercero y cuarto número es: " << producto << endl;
+    cout << "La suma de los dos primeros números es: " << suma << endl; // Muestra el resultado de la suma
+    cout << "El producto del tercero y cuarto número es: " << producto << endl; // Muestra el resultado del producto
     return 0;
 }
